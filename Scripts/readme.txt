284ebd70fd73decc7ff903115c6a77017f3d51fd
jahu00 <jahu00@users.noreply.github.com>
2015-03-29 23:48:49 +0200

    Configuration dependant package.json
    Also added dummy _references.js

---
+++ Scripts/readme.txt
@@ -0,0 +1,5 @@
+﻿File _references.js is used to add js code to intellisense.
+
+To use add following line to _references.js while replaceing the path with the path of the js file you want to include in intellisense.
+
+/// <reference path="script.js" />
---
+++ extend.ps1
@@ -0,0 +1,42 @@
+﻿if ($args.Length -eq 3)
+{
+	function ExtendJSON($base, $ext)
+	{
+		$propNames = $($ext | Get-Member -MemberType *Property).Name
+		foreach ($propName in $propNames) {
+			if ($base.PSObject.Properties.Match($propName).Count) {
+				if ($base.$propName.GetType().Name -eq "PSCustomObject")
+				{
+					$base.$propName = ExtendJSON $base.$propName $ext.$propName
+				}
+				else
+				{
+					$base.$propName = $ext.$propName
+				}
+			}
+			else
+			{
+				$base | Add-Member -MemberType NoteProperty -Name $propName -Value $ext.$propName
+			}
+		}
+		return $base
+	}
+	$file1 = (Get-Content $args[0]) -join "`n" | ConvertFrom-Json
+	$file2 = (Get-Content $args[1]) -join "`n" | ConvertFrom-Json
+	$output = ExtendJSON $file1 $file2 | ConvertTo-Json
+	#Save output as BOMless UTF8
+	[System.IO.File]::WriteAllLines($args[2], $output)
+}
+else
+{
+	echo "  --==:[extend.ps1 1.0]:==--"
+	echo "Combines two JSON files into one."
+	echo ""
+	echo "Usage:"
+	echo ""
+	echo "build.script file1 file2 target"
+	echo ""
+	echo "file1 - JSON file that will be used as a base"
+	echo "file2 - JSON file that will be used to extend file 1"
+	echo "target - name of the target JSON file"
+}
